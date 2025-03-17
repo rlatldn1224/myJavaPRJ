d561fe7a45238e68a5fa5344aee5830a3f86a528
rlatldn1224 <kswksw0322@naver.com>
2025-03-17 17:09:21 +0900

    Example1

---
+++ src/main/java/Ex1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ex1 {
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a+b);
+    }
+
+
+}

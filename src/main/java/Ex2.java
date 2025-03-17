478f6153ab494d3b1d180ae2478b678833cdb1da
rlatldn1224 <kswksw0322@naver.com>
2025-03-17 17:17:32 +0900

    Ex2

---
+++ src/main/java/Ex2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ex2 {
+
+    public static void main(Sting args[]){
+
+        Scanner sc =new Scanner(System.in);
+
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a-b);
+
+    }
+
+
+}

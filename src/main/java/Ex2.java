ac017f3faf91be792553229d0a65908d0e3df4b1
rlatldn1224 <kswksw0322@naver.com>
2025-03-17 17:37:33 +0900

    Ex2

--- src/main/java/Ex2.java
+++ src/main/java/Ex2.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;
 
-public class Ex2 {
+public class Ex1 {
 
-    public static void main(Sting args[]){
+    public static void main(String [] args){
 
-        Scanner sc =new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int a, b;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        a = sc.nextInt(3);
-        b = sc.nextInt(1);
-
-        System.out.println(a-b);
-
+        System.out.println(b - a);
     }
 
 

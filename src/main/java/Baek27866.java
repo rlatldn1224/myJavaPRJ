f341c267fec75863ab9e13adafb7ab6185344153
rlatldn1224 <kswksw0322@naver.com>
2025-03-21 09:30:43 +0900

    백준2745

---
+++ src/main/java/Baek27866.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Baek27866 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        int i = sc.nextInt();
+
+        System.out.println(str.charAt(i - 1));
+
+        sc.close();
+    }
+}

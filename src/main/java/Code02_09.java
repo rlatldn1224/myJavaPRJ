1f4709d50b711eb7c1b34c276cb903c5d3a1fed2
rlatldn1224 <kswksw0322@naver.com>
2025-03-24 12:45:32 +0900

    복습

---
+++ src/main/java/Code02_09.java
@@ -0,0 +1,22 @@
+public class Code02_09 {
+
+    public static void main(String[] args) {
+
+        int num1 = 10, num2 = 20;
+        int resAdd;
+        int resSub;
+        int resMul;
+        int resDiv;
+
+        resAdd = num1 + num2;
+        resSub = num1 - num2;
+        resMul = num1 * num2;
+        resDiv = num1 / num2;
+
+        System.out.println(num1 + "+" + num2 + "=" + resAdd);
+        System.out.println(num1 + "-" + num2 + "=" + resSub);
+        System.out.println(num1 + "*" + num2 + "=" + resMul);
+        System.out.println(num1 + "/" + num2 + "=" + resDiv);
+
+    }
+}

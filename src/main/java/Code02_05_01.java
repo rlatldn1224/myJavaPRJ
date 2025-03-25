b980ee38ccedf616802f6dabfdc41aeaed9f075f
rlatldn1224 <kswksw0322@naver.com>
2025-03-25 15:44:09 +0900

    복습

---
+++ src/main/java/Code02_05_01.java
@@ -0,0 +1,11 @@
+public class Code02_05_01 {
+
+    public static void main(String[] args) {
+
+        int a = 200;
+        int b = 300;
+
+        int c = a + b;
+        System.out.println(a +  "+" + b +  "=" + c);
+    }
+}

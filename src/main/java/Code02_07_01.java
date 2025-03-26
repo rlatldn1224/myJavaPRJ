f6d497cb3bc89acede6861e16689625997b54e63
rlatldn1224 <kswksw0322@naver.com>
2025-03-26 15:06:26 +0900

    복습

---
+++ src/main/java/Code02_07_01.java
@@ -0,0 +1,22 @@
+public class Code02_07_01 {
+
+    public static void main(String[] args) {
+
+        int num1, num2, result;
+        num1 = 3000;
+        num2 = 60;
+
+        result = num1 + num2;
+        System.out.println(num1 + "+" + num2 + "=" + result);
+
+        result = num1 - num2;
+        System.out.println(num1 + "-" + num2 + "=" + result);
+
+        result = num1 * num2;
+        System.out.println(num1 + "*" + num2 + "=" + result);
+
+        result = num1/num2;
+        System.out.println(num1 + "/" + num2 + "=" + result);
+
+    }
+}
---
+++ src/main/java/Code02_08_01.java
@@ -0,0 +1,14 @@
+public class Code02_08_01 {
+
+    public static void main(String[] args) {
+
+        String str1 = "난생처음";
+        String str2 = "자바";
+
+        System.out.println(str1.equals(str2));
+
+        String result = str1 + str2;
+
+        System.out.println(result);
+    }
+}

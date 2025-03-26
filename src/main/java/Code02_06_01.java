50353eb9a85f15093eac97a748d893158cce6306
rlatldn1224 <kswksw0322@naver.com>
2025-03-26 15:05:48 +0900

    복습

---
+++ src/main/java/Code02_06_01.java
@@ -0,0 +1,13 @@
+public class Code02_06_01 {
+
+    public static void main(String[] args) {
+        int num1, num2, result;
+
+        num1 = 100;
+        num2 = 300;
+
+        result = num1 + num2;
+        System.out.println(num1 + "+" + num2 + "=" + result);
+
+    }
+}

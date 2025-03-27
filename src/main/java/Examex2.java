6afe1992a62d29ab9efbe9b118cdec0f8f42ec92
rlatldn1224 <kswksw0322@naver.com>
2025-03-27 14:18:06 +0900

    복습

---
+++ src/main/java/Examex2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Examex2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+       int a = sc.nextInt();
+
+       int b = sc.nextInt();
+
+        double result = (double) a / b;
+        System.out.println(a + "/" + b + "=" + result);
+
+        sc.close();
+    }
+}

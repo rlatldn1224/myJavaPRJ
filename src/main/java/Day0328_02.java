52fbd69068a7737916d0d9d7ce2438dcfd4aedda
rlatldn1224 <kswksw0322@naver.com>
2025-03-28 11:19:53 +0900

    복습

---
+++ src/main/java/Day0328_02.java
@@ -0,0 +1,10 @@
+public class Day0328_02 {
+
+    public static void main(String[] args){
+
+        int num =10;
+        String text = "Java";
+        System.out.println(num);
+        System.out.println(text);
+    }
+}

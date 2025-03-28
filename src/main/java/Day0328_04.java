383d5caeb8c71c4c017ffdbd8d95b7ac55b396ad
rlatldn1224 <kswksw0322@naver.com>
2025-03-28 11:20:24 +0900

    복습

---
+++ src/main/java/Day0328_04.java
@@ -0,0 +1,10 @@
+public class Day0328_04 {
+    public static void main(String[] args) {
+        int x = -3;
+        if (x > 0) {
+            System.out.println("x는 양수입니다.");
+        }else {
+            System.out.println("x는 음수입니다. ");
+        }
+    }
+}

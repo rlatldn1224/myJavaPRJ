97537a218f4ddbe8c4b36bdf76279608e4c34985
rlatldn1224 <kswksw0322@naver.com>
2025-03-28 11:20:12 +0900

    복습

---
+++ src/main/java/Day0328_03.java
@@ -0,0 +1,9 @@
+public class Day0328_03 {
+    public static void main(String[] args) {
+
+        int x = 5;
+        if (x > 0) {
+            System.out.println("x는 0보다 큽니다.");
+        }
+    }
+}

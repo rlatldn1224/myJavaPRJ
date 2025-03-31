a4e0a0acf8c663a4f9675146504dd5c19f3da93d
rlatldn1224 <kswksw0322@naver.com>
2025-03-31 11:06:24 +0900

    복습

---
+++ src/main/java/Code03_02_01.java
@@ -0,0 +1,17 @@
+public class Code03_02_01 {
+    public static void main(String[] args) {
+        int n1, n2, res;
+        n1 = 5;
+        n2 = 3;
+        res = n1 + n2;
+        System.out.println(res);
+        res = n1 - n2;
+        System.out.println(res);
+        res = n1 * n2;
+        System.out.println(res);
+        res = n1 / n2;
+        System.out.println(res);
+        res = n1 % n2;
+        System.out.println(res);
+    }
+}

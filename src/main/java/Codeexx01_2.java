ada9990f77d9fc3ea9d6833fa4faff8885c93d7d
rlatldn1224 <kswksw0322@naver.com>
2025-04-01 17:22:33 +0900

    복습

---
+++ src/main/java/Codeexx01_2.java
@@ -0,0 +1,12 @@
+public class Codeexx01_2 {
+
+    public static void main(String[] args){
+
+        int a = 200;
+        int b = 300;
+
+        int c = a + b;
+        System.out.println(a + "+" + b + "=" + c);
+    }
+}
+

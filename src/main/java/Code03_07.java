b17a10265e992e4572169933344bf7ff28b2fffa
rlatldn1224 <kswksw0322@naver.com>
2025-04-02 13:40:18 +0900

    복습

---
+++ src/main/java/Code03_07.java
@@ -0,0 +1,9 @@
+public class Code03_07 {
+
+    public static void main(String[] args) {
+        int n;
+        n =100;
+        n = 100*200;
+        n = Integer.parseInt("100") + Integer.parseInt("200");
+    }
+}

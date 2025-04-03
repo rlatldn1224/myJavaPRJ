88563ef809631e32fce365d173ff6a4f9d80e67f
rlatldn1224 <kswksw0322@naver.com>
2025-04-03 11:02:07 +0900

    복습

---
+++ src/main/java/Code03_11.java
@@ -0,0 +1,15 @@
+public class Code03_11 {
+
+    public static void main(String[] args) {
+        int n1 = 100, n2 = 200;
+
+        System.out.println(n1 == n2);
+        System.out.println(n1 != n2);
+
+        System.out.println(n1 > n2);
+        System.out.println(n1 < n2);
+
+        System.out.println(n1 >= n2);
+        System.out.println(n1 <= n2);
+    }
+}

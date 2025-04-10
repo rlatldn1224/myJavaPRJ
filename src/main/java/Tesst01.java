5625c2bffc9d856fde5af5baea7cbe06513ca73f
rlatldn1224 <kswksw0322@naver.com>
2025-04-10 11:04:57 +0900

    복습

---
+++ src/main/java/Tesst01.java
@@ -0,0 +1,21 @@
+public class Tesst01 {
+
+    public static void main(String[] args){
+
+        int n=7;
+
+        for (int i = 0; i < n; i++){
+            if(i < n/2){
+                for(int j = 0; j <= i; j++){
+                    System.out.print("*");
+                }
+            }
+            else{
+                for(int j = n-1; j>=i; j--){
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

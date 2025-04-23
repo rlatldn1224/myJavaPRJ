e42ed097b37764c6b6f6327a0d5bffb1ba2c5c3e
rlatldn1224 <kswksw0322@naver.com>
2025-04-23 16:24:20 +0900

    복습

---
+++ src/main/java/Day0328_07.java
@@ -0,0 +1,10 @@
+public class Day0328_07 {
+
+    public static void main(String[] args) {
+
+        int[] num ={1, 2, 3,4, 5};
+        for (int i :num){
+            System.out.println(i);
+        }
+    }
+}

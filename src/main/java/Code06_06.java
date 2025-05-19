4e2b3f534c8719ec6f0227152f081ec8ad17fae3
rlatldn1224 <kswksw0322@naver.com>
2025-05-19 09:49:10 +0900

    복습

---
+++ src/main/java/Code06_06.java
@@ -0,0 +1,10 @@
+public class Code06_06 {
+    public static void main(String[] args) {
+        int hap;
+
+        for (int i = 1; i <= 10; i++) {
+            hap =  + i;
+        }
+        System.out.println("1부터 1000까지의 합계 : " );
+    }
+}

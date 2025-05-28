1c37a11334f20ca5acaa44ecba9ccbfcc8a29f9c
rlatldn1224 <kswksw0322@naver.com>
2025-05-29 08:46:31 +0900

    복습

---
+++ src/main/java/Exam01/Exam01_02.java
@@ -0,0 +1,19 @@
+package Exam01;
+
+public class Exam01_02 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 3, 7, 1, 4, 7, 3, 1, 9, 0, 7, 3};
+
+        int max = nums[0];
+
+        for (int i = 1; i < nums.length; i++){
+            if (nums[i]>max){
+                max = nums[i];
+            }
+        }
+
+        System.out.println(max +"입니다. ");
+    }
+}
